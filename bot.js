a81e040b2abdcf1118b488e06ad70ebbe5e692c1
Di4bel <40902128+Di4bel@users.noreply.github.com>
2018-07-13 22:50:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,9 @@ client.on('message', async message => {
     if (message.content === "firekeeper_soul") {
     	message.channel.send("…Ashen one, this is.. …much like what lies within me… Then let it find its own place, within my bosom. She will understand. We are both Fire Keepers, after all");
   	}
+     if (message.content === "eyes") {
+    	message.channel.send("...Ashen one, are these... Are these eyes? How gracious of thee, ashen one. The very things we Fire Keepers have been missing.");
+  	}
 
 
 
