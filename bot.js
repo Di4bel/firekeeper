e3c2184f4385336ba68dcd8ba4bcfd27cd78214f
Di4bel <40902128+Di4bel@users.noreply.github.com>
2018-07-13 21:55:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,21 +11,10 @@ client.on('message', message => {
     if (message.content === 'hello') {
     	message.channel.send('Welcome Home, ashen one. Speak thine heart's desire')
   	}
-});
-
-client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-if(cmd === `${prefix}hello`){
+ if(cmd === `hello`){
   return message.channel.send("Welcome Home, ashen one. Speak thine heart's desire");
 }
-if(cmd === `${prefix}firekeeper_soul`){
+  if(cmd === `firekeeper_soul`){
   return message.channel.send("…Ashen one, this is.. …much like what lies within me… Then let it find its own place, within my bosom. She will understand. We are both Fire Keepers, after all");
 }
 
