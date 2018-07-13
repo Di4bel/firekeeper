7624c5c95a57c08e9087d75c2ee812c37ff9dde9
Di4bel <40902128+Di4bel@users.noreply.github.com>
2018-07-13 21:38:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,13 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'hello') {
-    	message.channel.send('Welcome Home, ashen one. Speak thine heart's desire');
+    	message.channel.send('Welcome Home, ashen one. Speak thine heart's desire')
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'firekeeper_soul') {
-    	message.reply('…Ashen one, this is.. …much like what lies within me… Then let it find its own place, within my bosom. She will understand. We are both Fire Keepers, after all');
+    	message.reply('…Ashen one, this is.. …much like what lies within me… Then let it find its own place, within my bosom. She will understand. We are both Fire Keepers, after all')
   	}
 });
 
